f6db13c5d33c81ca984c5981aeabaff89daa9f40
Monte9 <Monte9@users.noreply.github.com>
2017-10-05 15:36:06 -0700

    added random color on modal cards

--- src/components/Cards/ModalCard.js
+++ src/components/Cards/ModalCard.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
 
+import { GRADIENTS } from '../Shared/Constants'
+
 export default class ModalCard extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      hover: false,
+      randomGradient: {
+        primary: '#D76D77',
+        secondary: '#FFAF7B'
+      }
+    }
+  }
+
+  toggleHover() {
+    const randomNumber = Math.floor(Math.random() * GRADIENTS.length);
+    const randomGradient = GRADIENTS[randomNumber]
+
+    this.setState({
+      hover: !this.state.hover,
+      randomGradient
+    })
+  }
+
   render() {
     const { action, label } = this.props
+    const { hover, randomGradient } = this.state
+    const linkStyle = {
+      background: `linear-gradient(145deg, ${randomGradient.primary}, ${randomGradient.secondary})`
+    }
 
     return (
-      <div className="GridBox GenericLink" onClick={action}>
-        <div className="BoxImage" />
+      <div className="GridBox" onClick={action}>
+        <div className="BoxImage"
+          style={linkStyle}
+          onMouseEnter={this.toggleHover.bind(this)}
+        />
         <div className="BoxLabel">{label}</div>
       </div>
     );
---
+++ src/components/Shared/Constants.js
@@ -0,0 +1,46 @@
+export const GRADIENTS = [
+  {
+    primary: '#34e89e',
+    secondary: '#0f3443'
+  },
+  {
+    primary: '#6190E8',
+    secondary: '#A7BFE8'
+  },
+  {
+    primary: '#c0c0aa',
+    secondary: '#1cefff'
+  },
+  {
+    primary: '#ef32d9',
+    secondary: '#89fffd'
+  },
+  {
+    primary: '#808080',
+    secondary: '#3fada8'
+  },
+  {
+    primary: '#3494E6',
+    secondary: '#EC6EAD'
+  },
+  {
+    primary: '#4ECDC4',
+    secondary: '#556270'
+  },
+  {
+    primary: '#43C6AC',
+    secondary: '#F8FFAE'
+  },
+  {
+    primary: '#bdc3c7',
+    secondary: '#2c3e50'
+  },
+  {
+    primary: '#D76D77',
+    secondary: '#FFAF7B'
+  },
+  {
+    primary: '#DBE6F6',
+    secondary: '#C5796D'
+  },
+]
